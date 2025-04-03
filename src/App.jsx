ff99c16b752f621dd7ef737d06497ae2dbfcceae
Shubham Tripathi <shubhamt5055@gmail.com>
2025-04-03 16:27:08 +0530

    create smart ai bot

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./assets/pages/Home";
+function App() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+function Home() {
+  return (
+    <div className="Home">
+      <nav>
+        <div className="logo">SmartAIBot</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
