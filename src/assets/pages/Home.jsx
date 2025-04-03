1342235ca82f2c986acf7c80de2cfd78a8d5c190
Shubham Tripathi <shubhamt5055@gmail.com>
2025-04-03 19:40:23 +0530

    some thing correctiion

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "../../App.css";
+import { RiImageAiLine } from "react-icons/ri";
+import { BiImageAdd } from "react-icons/bi";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { BsPlus } from "react-icons/bs";
+import { FaArrowUp } from "react-icons/fa6";
 function Home() {
   return (
-    <div className="Home">
+    <div className="home">
+      {/* home section */}
       <nav>
         <div className="logo">SmartAIBot</div>
       </nav>
+      <div className="hero">
+        <span id="tag">What can I help with ...?</span>
+        <div className="cate">
+          <div className="upImg">
+            <BiImageAdd />
+            <span>Upload Image</span>
+          </div>
+          <div className="genImg">
+            <RiImageAiLine />
+            <span>Generate Image</span>
+          </div>
+          <div className="chat">
+            <IoChatbubbleEllipsesOutline />
+            <span>Let's Chat</span>
+          </div>
+        </div>
+      </div>
+
+      {/* input section */}
+      <form className="input-box">
+        <button id="add">
+          <BsPlus />
+        </button>
+        <input type="text" placeholder="Ask Something..." />
+        <button id="submit">
+          <FaArrowUp />
+        </button>
+      </form>
     </div>
   );
 }
